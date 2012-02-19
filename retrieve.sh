0a4612f3811a14624143dce1095690713a0d0d92
Russell Myers <mezner@russellmyers.com>
2012-02-18 22:20:33 -0500

    Adding global gitignore to retrieve.sh

--- retrieve.sh
+++ retrieve.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Script to pull the current user configuration into the current directory.
+
+# Git-related files
+cp -v ~/.gitignore_global .
 cp -v ~/.gitconfig .
+
 cp -v ~/.inputrc .
 cp -v ~/.vimrc .
 cp -v ~/.bash_profile .
