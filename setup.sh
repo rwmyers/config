b275124b45c262a17a681890fbc5c61e100c4dd4
Russell Myers <mezner@russellmyers.com>
2016-09-24 12:50:59 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ echo 'installing applications'
 sudo apt-get install git
 sudo apt-get install zsh
 sudo chsh /bin/zsh
+cat ~/.ssh/id_rsa.pub
+read -p "Put the token in github and press any key to continue"
 echo 'pulling configurations'
-git clone https://github.com/Mezner/Personal-Configurations.git configs
+git clone git@github.com:Mezner/Personal-Configurations.git configs
 cd ~/src/configs
 cp .zshrc ~/
 cd ~
-git clone https://github.com/Mezner/oh-my-zsh.git .oh-my-zsh
+git clone git@github.com:Mezner/oh-my-zsh.git.oh-my-zsh
 mkdir ~/bin
 echo 'git config'
 cp git/.git* ~/
@@ -19,5 +21,3 @@ cp -rf vim/.vim* ~/
 echo 'bash config'
 cp -rf bash/.bash* ~/
 ssh-keygen -N ""
-cat ~/.ssh/id_rsa.pub
-echo 'USE THE ABOVE IN GIT HOSTS!'
