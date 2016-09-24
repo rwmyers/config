10bda4774793637157454361b26a00bdbdb5b8a8
Russell Myers <mezner@russellmyers.com>
2016-09-24 12:32:38 -0400

    Add simple setup script for bash ubuntu

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mkdir ~/src
+cd ~/src
+echo 'installing applications'
+sudo apt-get install git
+sudo apt-get install zsh
+sudo chsh /bin/zsh
+echo 'pulling configurations'
+git clone https://github.com/Mezner/Personal-Configurations.git configs
+cd ~/src/configs
+cp .zshrc ~/
+cd ~
+git clone https://github.com/Mezner/oh-my-zsh.git .oh-my-zsh
+mkdir ~/bin
+echo 'git config'
+cp git/.git* ~/
+echo 'vim config'
+cp -rf vim/.vim* ~/
+echo 'bash config'
+cp -rf bash/.bash* ~/
