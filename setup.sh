001db48cb7a5ef3921a6776ca5f23364b5e4d117
Russell Myers <mezner@russellmyers.com>
2020-01-02 14:30:06 -0500

    New, improved, resilient setup script. It's ZSH.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,83 @@
-#!/bin/sh
-mkdir ~/src
-cd ~/src
-echo 'installing applications'
-sudo apt-get install git
-sudo apt-get install zsh
-sudo chsh /bin/zsh
-cat ~/.ssh/id_rsa.pub
-read -p "Put the token in github and press any key to continue"
-echo 'pulling configurations'
-git clone git@github.com:Mezner/Personal-Configurations.git configs
-cd ~/src/configs
-cp .zshrc ~/
-cd ~
-git clone git@github.com:Mezner/oh-my-zsh.git.oh-my-zsh
-mkdir ~/bin
-echo 'git config'
-cp git/.git* ~/
-echo 'vim config'
-cp -rf vim/.vim* ~/
-echo 'bash config'
-cp -rf bash/.bash* ~/
-ssh-keygen -N ""
+if ! type "git" > /dev/null; 
+then
+    echo "Install git!"
+    echo "Mac:    xcode-select --install"
+    exit 1
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]];
+then
+    if ! type brew > /dev/null;
+    then
+        echo "Installing Brew on OSX"
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        echo "Updating brew"
+        brew update
+    fi
+fi
+
+if ! cat ~/.ssh/id_rsa.pub > /dev/null; 
+then
+    echo "No keyfile detected. Running SSH-keygen"
+    ssh-keygen
+    echo ""
+    cat ~/.ssh/id_rsa.pub
+    echo ""
+    read -p "Put the token in github and press any key to continue"
+fi
+
+if [ ! -d "$HOME/src/" ] 
+then
+    mkdir -p ~/src/ 
+fi
+
+if [ ! -d "$HOME/src/config" ]
+then
+    pushd ~/src > /dev/null
+    git clone git@github.com:Mezner/config.git
+    popd > /dev/null
+else
+    pushd ~/src/config > /dev/null
+    git pull
+    popd > /dev/null
+fi
+
+
+if ! type "tmux" > /dev/null;
+then
+    if [[ "$OSTYPE" == "darwin"* ]];
+    then
+        brew update
+        brew install tmux
+    fi
+fi
+
+if [ ! -f "$HOME/.tmux.conf" ]
+then
+    ln -s ~/src/config/.tmux.conf ~/.tmux.conf
+fi
+
+if [ ! -f "$HOME/.tmux.conf.local" ]
+then
+    ln -s ~/src/config/.tmux.conf.local ~/.tmux.conf.local
+fi
+
+
+if [ ! -f "$HOME/.vimrc" ]
+then
+    ln -s ~/src/config/vim/.vimrc ~/.vimrc
+fi
+
+if [ ! -d "$HOME/.vim" ]
+then
+    ln -s ~/src/config/vim/.vim ~/.vim
+fi
+
+if [ ! -d "$HOME/.config/alacritty" ]
+then
+    mkdir -p ~/.config/alacritty
+    ln -s ~/src/config/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+fi
+
+
