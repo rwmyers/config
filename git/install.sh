5ae6cb9c302a0155e584a91476ff030ade55f30d
Russell Myers <mezner@russellmyers.com>
2021-05-08 18:51:39 -0400

    Remove unneccessary install.sh

--- git/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-git clone https://github.com/Mezner/gitscripts.git
-echo "# git scripts" >> ~/.zshrc
-echo "# git scripts" >> ~/.bashrc
-echo "export \"PATH=\$PATH:$(pwd)/gitscripts/\"" >> ~/.zshrc
-echo "export \"PATH=\$PATH:$(pwd)/gitscripts/\"" >> ~/.bashrc
