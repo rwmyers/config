eda9d018486f9de792267bc3f4c8e58ff3346563
Russell Myers <mezner@russellmyers.com>
2024-07-22 19:09:18 -0400

    Restart waybar service if exists, not if it's already started.
    The previous implementation would cause waybar to independently spawn
    because the first evaluation would evaluate that the waybar service
    wasn't running, then start independently, while subsequent runs would
    somehow kick up the service.

--- .config/sway/scripts/restart_waybar.sh
+++ .config/sway/scripts/restart_waybar.sh
@@ -2,9 +2,10 @@
 
 service_name=waybar.service
 
-if systemctl --user is-active --quiet "$service_name"; then
-    systemctl --user stop "$service_name"
-    systemctl --user start "$service_name"
+if systemctl --user list-unit-files --quiet "$service_name"; then
+    echo "service exists, restarting"
+    systemctl --user restart "$service_name"
 else
+    echo "service does not exist, executing waybar directly"
     pkill -x waybar; waybar
 fi
