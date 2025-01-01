c6a85dc07bef16041abb455ad934e870bce359d0
Russell Myers <mezner@russellmyers.com>
2024-12-31 19:58:50 -0500

    Automatic cd in zsh.

--- .config/zsh/setopt.zsh
+++ .config/zsh/setopt.zsh
@@ -1,6 +1,9 @@
 # === Beeping
 setopt no_beep               # Turn off the bell
 
+# === Navigation
+setopt auto_cd               # If not a normal command and it's the name of a directory, try to cd
+
 # === History
 setopt share_history         # Share command history data
 setopt append_history        # Allow multiple sessions to append to one command history
