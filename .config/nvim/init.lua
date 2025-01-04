68c58cd8dd2bb1b40157b0821b23591bbb0219f3
Russell Myers <mezner@russellmyers.com>
2025-01-04 10:08:31 -0500

    Create neovim separate config.
    This isn't yet linked up because I need to somehow detangle existing
    setups.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,31 @@
+-- VIM Configuration File
+
+-- allows backspace over anything
+vim.opt.backspace = { 'indent', 'eol', 'start' }
+
+-- show whitespace
+vim.opt.list = true
+vim.opt.listchars = { eol = '$', tab = '>-', trail = '~', extends = '>', precedes = '<' }
+
+-- show line numbers
+vim.opt.number = true
+
+-- tabs and auto-indent
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- show status bar
+vim.opt.laststatus = 2
+
+vim.cmd('colorscheme molokai')
+
+-- syntax
+vim.opt.syntax = 'on'
+
+vim.keymap.set('n', '<M-Right>', ':bn!<CR>')
+vim.keymap.set('n', '<M-Left>', ':bp!<CR>')
+
+-- filetypes
+vim.opt.filetype = 'on'
