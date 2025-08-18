6ad33a4ef749c8ff72a3fb48cc8c63282892ae6e
Russell Myers <mezner@russellmyers.com>
2025-08-17 22:28:44 -0400

    Extract font installs into fonts.sh

---
+++ install/fonts.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# Fonts
+FONT_DIR="$HOME/.local/share/fonts"
+mkdir -p "$FONT_DIR"
+
+# The (N) is a Zsh "glob qualifier" that makes the pattern
+# expand to an empty list if no files match (null glob).
+files=("$FONT_DIR"/CaskaydiaMono*(N))
+
+if (( ${#files[@]} == 0 )); then
+    print_note "Downloading CaskaydiaMono Nerd Fonts"
+    font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CascadiaMono.zip"
+    font_tmp_dir="$HOME/tmp_fonts"
+    mkdir -p $font_tmp_dir
+    pushd $font_tmp_dir
+    wget $font_url
+    unzip -o CascadiaMono.zip -d $FONT_DIR
+    popd $font_tmp_dir
+    rm -rf $font_tmp_dir
+fi
