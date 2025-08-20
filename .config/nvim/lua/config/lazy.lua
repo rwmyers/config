7ae6e14b5917db4baf9ed94c9b1daa0f25476d11
Russell Myers <mezner@russellmyers.com>
2025-08-20 00:07:18 -0400

    Remove hardcoded nvim setup, replace with symbolic link

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -28,10 +28,6 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
-
-vim.cmd.colorscheme "catppuccin"
---
+++ .config/nvim/lua/plugins/neovim.lua
@@ -0,0 +1,1 @@
+../../../../../../themes/current/neovim.lua
