90d841ef3da6648fe761b58bc3fce2a22d4536f9
Russell Myers <mezner@russellmyers.com>
2025-12-31 14:58:14 -0500

    Reconfigure file explorer key

--- .config/nvim/lua/plugins/fzf-lua-explorer.lua
+++ .config/nvim/lua/plugins/fzf-lua-explorer.lua
@@ -2,7 +2,7 @@ return {
 	"otavioschwanck/fzf-lua-explorer.nvim",
 	dependencies = { "ibhagwan/fzf-lua" },
 	keys = {
-		{ "<leader>.", "<cmd>lua require('fzf-lua-explorer').explorer()<cr>", desc = "Explorer" },
+		{ "<C-S-n>", "<cmd>lua require('fzf-lua-explorer').explorer()<cr>", desc = "Explorer" },
 	},
 	config = function()
 		require("fzf-lua-explorer").setup()
