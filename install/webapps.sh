1de10f1b116be1139e6dbb565c107241f4cb4761
Russell Myers <mezner@russellmyers.com>
2026-01-04 13:53:58 -0500

    Google messages install.

--- install/webapps.sh
+++ install/webapps.sh
@@ -5,3 +5,4 @@ source install/common.sh
 print_note "Installing webapps"
 
 webapp-install "Gmail" "https://mail.google.com/mail" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gmail.png"
+webapp-install "Messages" "https://messages.google.com/web/" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-messages.png"
