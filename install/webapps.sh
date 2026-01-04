b6b56b27eaeab2d718b1bdd7766c8497ab8dc2f4
Russell Myers <mezner@russellmyers.com>
2026-01-04 13:56:52 -0500

    Install Gemini webapp

--- install/webapps.sh
+++ install/webapps.sh
@@ -6,3 +6,4 @@ print_note "Installing webapps"
 
 webapp-install "Gmail" "https://mail.google.com/mail" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gmail.png"
 webapp-install "Messages" "https://messages.google.com/web/" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-messages.png"
+webapp-install "Gemini" "https://gemini.google.com/app" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-gemini.png"
