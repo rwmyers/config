96800f4f4f0f2532d947e82d8b2872c732664814
Russell Myers <mezner@russellmyers.com>
2026-01-05 10:48:13 -0500

    Install bluetui dependency.

--- install/env/bluetui.sh
+++ install/env/bluetui.sh
@@ -6,5 +6,8 @@ source $HOME/src/config/install/common.sh
 if [ ! -e "$HOME/.cargo/bin/bluetui" ]
 then
     print_note "Installing bluetui"
+    # Dependencies of bluetui
+    sudo apt install libdbus-1-dev
+
     cargo install bluetui 
 fi
