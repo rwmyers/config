d0034468c6098f6e767de71c989c28642f724493
Russell Myers <mezner@russellmyers.com>
2026-03-29 22:17:55 -0400

    Add description and link to delta install script

--- install/development/delta.sh
+++ install/development/delta.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# delta - A syntax-highlighting pager for git diffs, blame, and grep output
+# https://github.com/dandavison/delta
+
 source $HOME/src/config/install/common.sh
 
 install_linux_package delta git-delta
