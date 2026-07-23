3a1c77011fbd8cee2fe1ddb56a338dff515b8802
Russell Myers <mezner@russellmyers.com>
2026-07-22 21:15:18 -0400

    Tweak wezterm styling.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("CaskaydiaMono Nerd Font")
-config.font_size = 11
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 13
 
-config.color_scheme = "Catppuccin Macchiato"
-config.window_background_opacity = 0.95
+config.color_scheme = "rose-pine"
+config.window_background_opacity = 0.9
 
 config.scrollback_lines = 90000
 config.hide_tab_bar_if_only_one_tab = true
