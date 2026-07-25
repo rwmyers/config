714850cb520bdd3c13894e9e6a98dd0920384513
Russell Myers <mezner@russellmyers.com>
2026-07-25 09:06:21 -0400

    Stop tracking changes to the per-device themes/current symlink

--- install/themes.sh
+++ install/themes.sh
@@ -6,6 +6,9 @@ print_note "Installing themes"
 
 ln -snf $SRC_ROOT/themes $HOME/themes
 
+# themes/current is per-device, so stop git tracking changes to it here
+git -C $SRC_ROOT update-index --skip-worktree themes/current
+
 # btop theme linkage
 mkdir -p $HOME/.config/btop/themes
 ln -snf $HOME/themes/current/btop.theme $HOME/.config/btop/themes/current.theme
